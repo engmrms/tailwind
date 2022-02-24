23780e75aa1323b6a1c09458204ddebdc968025b
Mohammed Saleh <mrabah@tetco.sa>
2022-02-24 16:42:56 +0300

    initial

---
+++ gulpfile.js
@@ -0,0 +1,44 @@
+const { watch, series, src, dest } = require("gulp");
+var browserSync = require("browser-sync").create();
+var postcss = require("gulp-postcss");
+//const imagemin = require("gulp-imagemin");
+
+// Task for compiling our CSS files using PostCSS
+function cssTask(cb) {
+    return src("./src/*.css") // read .css files from ./src/ folder
+        .pipe(postcss()) // compile using postcss
+        .pipe(dest("./assets/css")) // paste them in ./assets/css folder
+        .pipe(browserSync.stream());
+}
+
+// Task for minifying images
+// function imageminTask(cb) {
+//     return src("./assets/images/*")
+//         .pipe(imagemin())
+//         .pipe(dest("./assets/images"));
+// }
+
+// Serve from browserSync server
+function browsersyncServe(cb) {
+    browserSync.init({
+        server: {
+            baseDir: "./",
+        },
+    });
+    cb();
+}
+
+function browsersyncReload(cb) {
+    browserSync.reload();
+    cb();
+}
+
+// Watch Files & Reload browser after tasks
+function watchTask() {
+    watch(["./src/*.css","./**/*.html"], series(cssTask, browsersyncReload));
+}
+
+// Default Gulp Task
+exports.default = series(cssTask, browsersyncServe, watchTask);
+exports.css = cssTask;
+//exports.images = imageminTask;
---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: ["./src/**/*.css",'./*.html'],
+
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [
+    //require('@tailwindcss/aspect-ratio'),
+  ],
+}
